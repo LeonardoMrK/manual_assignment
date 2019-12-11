dac5c2c03675d1d57edfe7fa35486f2a4759f18e
LeonardiMrK <cmccedeccmc@163.com>
2019-12-11 21:51:34 +0800

    对标注后的数据进行了整理，生成了分好类的数据

--- tag_assignment_processor.py
+++ tag_assignment_processor.py
@@ -7,27 +7,32 @@
 #0标签是食物，1标签是添加物，2是其他
 
 import re
-output_path='D:\project\pycharmworkspace\\tag_assignment\output\\'
+output_path='D:\\project\\pycharmworkspace\\tag_assignment\\output\\'
+input_path='D:\\project\\pycharmworkspace\\tag_assignment\\tag_data_V1.0\\'
 
-with open("tag_data_V1.0/0.txt",'r',encoding='utf-8') as f:
-    while True:
-        line = f.readline()
-        # print(line)
-        if not line:
-            break
-        elif re.match(r'0.*',line):#正则匹配0标签的内容（食品）
-            f1 = open(output_path+'0.txt', 'a')#新建文本文件
-            f1.write(line)#写入
-            f1.close()#一定要关闭，不然写不进去
-        elif re.match(r'1.*',line):#正则匹配1标签的内容（添加剂）
-            f2 = open(output_path+'1.txt', 'a')
-            f2.write(line)
-            f2.close()
-        elif re.match(r'2.*',line):#正则匹配2标签的内容（其他）
-            f3=open(output_path+'2.txt','a')
-            f3.write(line)
-            f3.close()
-        else:  #无标签句子
-            f4 = open(output_path + '3.txt', 'a')
-            f4.write(line)
-            f4.close()
+for i in range(4):
+    num=i+1
+    with open(input_path+str(num)+'.txt','r',encoding='utf-8') as f:
+        while True:
+            line = f.readline()
+            # print(line)
+            if not line:
+                break
+            elif re.match(r'0.*',line):#正则匹配0标签的内容（食品）
+                f1 = open(output_path+'0.txt', 'a')#新建文本文件
+                f1.write(line)#写入
+                f1.close()#一定要关闭，不然写不进去
+            elif re.match(r'1.*',line):#正则匹配1标签的内容（添加剂）
+                f2 = open(output_path+'1.txt', 'a')
+                f2.write(line)
+                f2.close()
+            elif re.match(r'2.*',line):#正则匹配2标签的内容（其他）
+                f3=open(output_path+'2.txt','a')
+                f3.write(line)
+                f3.close()
+            else:  #无标签句子
+                f4 = open(output_path + '3.txt', 'a')
+                f4.write(line)
+                f4.close()
+
+print("Process done")
